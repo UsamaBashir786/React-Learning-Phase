c9d2d266e25fbac1a0082a5f4ea59dc7c5878fda
Usama Bashir <165399383+UsamaBashir786@users.noreply.github.com>
2026-03-06 01:41:34 +0500

    How setup tailwind css

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-gray-900">
+      <div className="text-2xl text-white text-center p-10">App</div>
+    </div>
+  );
+};
+
+export default App;
